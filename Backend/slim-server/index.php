628506c5bc4acdaa6ab732086f84a08da862097e
Leon Kühn <leonkuehn9@gmail.com>
2021-08-26 14:43:38 +0200

    added database queries required by getlistview endpoint

--- Backend/slim-server/index.php
+++ Backend/slim-server/index.php
@@ -33,14 +33,44 @@ $app->PUT('/Calendar/processAbsence', function($request, $response, $args) {
  */
 $app->GET('/Calendar/returnListview', function($request, $response, $args) {
             
+            $queryParams = $request->getQueryParams();
+			//TODO: Cannot find course in any model, add it!
+			$course = $queryParams['course'];
+			
+			//TODO: DBConnection Ausfüllen
+            $con = mysqli_connect('host','database','user','password');
+            if(!$con) {
+                die('Could not connect: ' . mysqli_error($con));
+            }
             
+			//TODO: Documentation states names of the database, not table, as second parameter. Verify / change if required
+			mysqli_select_db($con,"d02c66a3");
+			$sqlPrep="select * from hat where 'ID_Studiengruppe'='$course'";
+			$resultPrep = mysqli_query($con,$sqlPrep); 
+			while ($rowPrep = mysql_fetch_array($resultPrep)) {
+				$moduleID = utf8_encode($rowPrep['ID_Modul']);
+				$sql="select * from Vorlesungen where 'ID_Modul'='$moduleID'",
+				$result = mysqli_query($con,$sql);
+				$row = mysqli_fetch_assoc($result));
+				
+				// get Name of corresponding Module to display
+				$sqlModuleName = "Select * from Modul where 'ID_Modul'='$moduleID'"
+				$resultModuleName = mysqli_query($con,$sqlModuleName); 
+				$rowModuleName = mysqli_fetch_assoc($resultModuleName));
+				
+				// Data to display 
+				$moduleName = utf8_encode($rowModuleName['Name']);
+				$start = utf8_encode($row['Beginn']); 
+				$end = utf8_encode($row['Ende']); 
+				$protocol = utf8_encode($row['Prot']);
+				
+				//TODO: Add moduleName, start, end, protocol to array of Events to display / convert to json
+			}
             
-            $body = $request->getParsedBody();
             $response->write('How about implementing returnListView as a GET method ?');
             return $response;
             });
-
-
+			
 /**
  * GET getUserInfo
  * Summary: Get Info about User
