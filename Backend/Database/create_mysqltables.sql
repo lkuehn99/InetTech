1bfbc311dca027fee1aa7e075087e9a2badbc7cc
Dennis Suffel <80106155+DennisSuffel@users.noreply.github.com>
2021-08-30 09:45:08 +0200

    Correct sql statements
    added more entries and corrected data inconsistencies.

--- Backend/Database/create_mysqltables.sql
+++ Backend/Database/create_mysqltables.sql
@@ -36,8 +36,7 @@ CREATE Table hat(
 
 INSERT INTO Studiengruppen(ID_Studiengruppe, Name)
 VALUES(1, 'WS-18-II'),
-(2, 'WS-19-II'),
-(3, 'WS-20-II');
+(2, 'WS-19-II');
 
 INSERT INTO Modul(ID_Modul, Name)
 VALUES(1, 'Datenbanken'),
@@ -48,18 +47,20 @@ INSERT INTO Benutzer(Benutzername, Vorname, Nachname, rolle, Passwort, ID_Studie
 VALUES('treinig','Thomas','Reinig','Student','4fcaf07c3790bd48a564ef8e05c7abdc',1),
 ('nengel','Niklas','Engel','Student','17ac5400676655300e7898906fbfcf33',1),
 ('lkuehn','Leon','Kuehn','Student','0ed260a8f7f8c7534859e1edd52337a0',2),
-('fvoss','Frederik','Voss','Student','692359073981e56c2b10ab2d592ac2f9',3),
+('fvoss','Frederik','Voss','Student','692359073981e56c2b10ab2d592ac2f9',1),
 ('dsuffel','Dennis','Suffel','Student','51782ea7286c92803385c67f667155c1',2);
 
 INSERT INTO Vorlesungen(ID_Vorlesung, Beginn, Ende, Prot, ID_Modul)
-VALUES(1, '2021-10-01 08:30:00', '2021-10-01 11:45:00', 'treinig', 2),
-(2, '2021-10-02 08:30:00', '2021-10-02 11:45:00', 'nengel', 1),
-(3, '2021-10-03 08:30:00', '2021-10-03 11:45:00', 'lkuehn', 3),
-(4, '2021-10-06 12:45:00', '2021-10-06 16:00:00', 'fvoss', 1),
-(5, '2021-10-07 12:45:00', '2021-10-07 16:00:00', 'dsuffel', 2);
+VALUES(1, '2021-10-01 08:30:00', '2021-10-01 11:45:00', 'nengel', 2),
+(2, '2021-10-02 08:30:00', '2021-10-02 11:45:00', 'lkuehn', 1),
+(3, '2021-10-03 08:30:00', '2021-10-03 11:45:00', 'treinig', 3),
+(4, '2021-10-06 12:45:00', '2021-10-06 16:00:00', 'dsuffel', 1),
+(5, '2021-10-07 12:45:00', '2021-10-07 16:00:00', 'fvoss', 2),
+(6, '2021-10-08 12:45:00', '2021-10-07 16:00:00', 'nengel', 3),
+(7, '2021-10-09 08:30:00', '2021-10-03 11:45:00', 'treinig', 3);
 
 INSERT INTO hat(ID_Studiengruppe, ID_Modul)
 VALUES(1,3),
 (2,1),
-(3,2),
-(2,3);
+(1,2),
+(1,1);
